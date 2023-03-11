a14a9d7b51ac38e72188c7ac5a8714220ae78d12
Igor Bzdawka <83597859+AjgorBzd@users.noreply.github.com>
2023-03-11 21:51:56 +0100

    Add files via upload

---
+++ WpfSlideshowApp/HorizontalEffect/HorizontalEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+namespace WpfSlideshowApp
+{
+    public class HorizontalEffect : ISlideshowEffect
+    {
+        public string Name => "Horizontal effect";
+
+        public void PlaySlideshow(Image imageIn, Image imageOut, double windowWidth, double windowHeight)
+        {
+
+        }
+    }
+}
